256a081c8936618578d3e73031872f39497fefb2
Daryaskhrv <daryasheqer@yandex.ru>
2024-11-27 15:10:13 +0400

    настройка wrapper над автоматически сгенерированным клиентом

---
+++ HotelBookingSystem/HotelBookingSystem.Client/Api/HotelBookingSystemApiWrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingSystem.Client.Api;
+
+public class HotelBookingSystemApiWrapper(IConfiguration configuration) : IHotelBookingSystemApiWrapper
+{
+    public readonly HotelBookingSystemApi _client = new(configuration["OpenApi:ServerUrl"], new HttpClient());
+
+    public async Task<ICollection<HotelClientGetDto>> GetAllClients() => await _client.HotelClientAllAsync();
+    public async Task CreateClient(HotelClientPostDto clientDto) => await _client.HotelClientPOSTAsync(clientDto);
+    public async Task<HotelClientGetDto> GetClientById(int id) => await _client.HotelClientGETAsync(id);
+    public async Task<HotelClientGetDto> UpdateClient(int id, HotelClientPostDto clientDto) => await _client.HotelClientPUTAsync(id, clientDto);
+    public async Task DeleteClient(int id) => await _client.HotelClientDELETEAsync(id);
+    public async Task<ICollection<HotelClientGetDto>> GetClientsByHotel(string hotelName) => await _client.HotelAsync(hotelName);
+    public async Task<ICollection<HotelClientGetDto>> GetLongestRentalClients() => await _client.LongestRentalAsync();
+    
+}
---
+++ HotelBookingSystem/HotelBookingSystem.Client/Api/IHotelBookingSystemApiWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingSystem.Client.Api;
+
+public interface IHotelBookingSystemApiWrapper
+{
+    Task<ICollection<HotelClientGetDto>> GetAllClients();
+    Task CreateClient(HotelClientPostDto clientDto);
+    Task<HotelClientGetDto> GetClientById(int id);
+    Task<HotelClientGetDto> UpdateClient(int id, HotelClientPostDto clientDto);
+    Task DeleteClient(int id);
+    Task<ICollection<HotelClientGetDto>> GetClientsByHotel(string hotelName);
+    Task<ICollection<HotelClientGetDto>> GetLongestRentalClients();
+}

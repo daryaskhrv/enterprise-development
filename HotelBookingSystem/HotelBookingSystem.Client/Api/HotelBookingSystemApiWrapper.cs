176b423123c9f513240e548040994b7f7e9174d0
Daryaskhrv <daryasheqer@yandex.ru>
2024-11-30 13:39:00 +0400

    изменение переменных

--- HotelBookingSystem/HotelBookingSystem.Client/Api/HotelBookingSystemApiWrapper.cs
+++ HotelBookingSystem/HotelBookingSystem.Client/Api/HotelBookingSystemApiWrapper.cs
@@ -2,15 +2,15 @@
 
 public class HotelBookingSystemApiWrapper(IConfiguration configuration) : IHotelBookingSystemApiWrapper
 {
-    public readonly HotelBookingSystemApi _client = new(configuration["OpenApi:ServerUrl"], new HttpClient());
+    public readonly HotelBookingSystemApi Client = new(configuration["OpenApi:ServerUrl"], new HttpClient());
 
-    public async Task<ICollection<HotelGetDto>> GetAllHotels() => await _client.AllHotelsAsync();
+    public async Task<ICollection<HotelGetDto>> GetAllHotels() => await Client.AllHotelsAsync();
 
-    public async Task<HotelGetDto> GetHotelById(int id) => await _client.HotelGETAsync(id);
+    public async Task<HotelGetDto> GetHotelById(int id) => await Client.HotelGETAsync(id);
 
-    public async Task<HotelGetDto> UpdateHotel(int id, HotelPostDto hotelDto) => await _client.HotelPUTAsync(id, hotelDto);
+    public async Task<HotelGetDto> UpdateHotel(int id, HotelPostDto hotelDto) => await Client.HotelPUTAsync(id, hotelDto);
 
-    public async Task DeleteHotel(int id) => await _client.HotelDELETEAsync(id);
+    public async Task DeleteHotel(int id) => await Client.HotelDELETEAsync(id);
 
-    public async Task CreateHotel(HotelPostDto hotelDto) => await _client.HotelPOSTAsync(hotelDto);
+    public async Task CreateHotel(HotelPostDto hotelDto) => await Client.HotelPOSTAsync(hotelDto);
 }
